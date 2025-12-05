f97fa852e3a90b225573e6264b667a67e5939d78
Edvard Pedersen <edvard.pedersen@uit.no>
2025-12-05 08:30:31 +0100

    untested changes to 1

--- 2025/1/src/main.zig
+++ 2025/1/src/main.zig
@@ -13,13 +13,12 @@ pub fn main() !void {
         const real_line = line orelse break;
         const s_dir = real_line[0];
         const s_num = real_line[1..];
-        const num = try std.fmt.parseInt(u32, s_num, 10);
+        const num : u32 = try std.fmt.parseInt(u32, s_num, 10);
         var direction:i32 = 1;
         if(s_dir == 'L') direction = -1;
-        for(0..num) {
+        for(0..num) |_| {
            current_val += direction;
-           if(current_val == -1) current_val = 99;
-           if(current_val == 100) current_val = 0;
+           current_val = @mod(current_val, 100);
            if(current_val == 0) sum_null += 1;
         }
     } else |err| {
